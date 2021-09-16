de5d8e98ef9351ace1d11f55c70b36360175d3b9
redxzeta <xzetanzero@gmail.com>
2021-09-15 20:12:13 -0700

    first commit

---
+++ src/main/java/com/golden/experience/ExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.golden.experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperienceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/golden/experience/ExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golden.experience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
